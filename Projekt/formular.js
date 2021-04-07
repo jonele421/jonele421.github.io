6882f6c647f48c446d17fc5002a9f65c6ffecf77
jonele421 <70194198+jonele421@users.noreply.github.com>
2021-04-07 13:05:32 +0200

    Add files via upload

---
+++ Projekt/formular.js
@@ -0,0 +1,12 @@
+function ifunction(){
+        var age = document.getElementById("age").value;
+        var lname = document.getElementById("lname").value;
+        var fname = document.getElementById("fname").value;
+        var mail = document.getElementById("mail").value;
+            document.getElementById("text").innerHTML = "<p>Your information is the following: <br> First name: " +fname +" <br> Last name: " 
+            +lname +"<br> Age: "+age+"<br> Mail: "+mail+  "<br><br> You are now subscribing to our newsletter and a succesful member at Jens' Coffee!</p>";
+            
+        
+
+
+}
